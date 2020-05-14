4ba85798b44bbb4b7dc25f0892647226651781a7
Sourav Sinha <saurabhkumarsinha81@gmail.com>
2020-05-14 10:59:32 +0530

    Initial commit

---
+++ Flask.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request
+import sys
+
+
+class Graph():
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printSolution(self, dist,y):
+        #print("Vertex \tDistance from Source")
+        #print(dist[y])
+        return(dist[y])
+
+    def minDistance(self, dist, sptSet):
+
+        # Initilaize minimum distance for next node
+        min = sys.maxsize
+
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
+
+    def dijkstra(self, src,y):
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and sptSet[v] == False and \
+                        dist[v] > dist[u] + self.graph[u][v]:
+                    dist[v] = dist[u] + self.graph[u][v]
+
+        return self.printSolution(dist,y)
+
+g = Graph(9)
+g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+           [4, 0, 8, 0, 0, 0, 0, 11, 0],
+           [0, 8, 0, 7, 0, 4, 0, 0, 2],
+           [0, 0, 7, 0, 9, 14, 0, 0, 0],
+           [0, 0, 0, 9, 0, 10, 0, 0, 0],
+           [0, 0, 4, 14, 10, 0, 2, 0, 0],
+           [0, 0, 0, 0, 0, 2, 0, 1, 6],
+           [8, 11, 0, 0, 0, 0, 1, 0, 7],
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]
+           ];
+
+
+app1=Flask(__name__)
+@app1.route('/')
+def hello():
+    return render_template('Home.html')
+
+loc=0
+des=0
+@app1.route('/', methods=['POST'])
+def submit_data():
+    if request.method=='POST':
+        loc=int(request.form['username'])
+        des=int(request.form['age1'])
+
+        x = loc
+        y = des
+        k=g.dijkstra(x,y)
+
+    return render_template('Home.html',loc1=k,des=des)
+    #return "<h1>Hello {} {}".format(loc,des)
+
+if __name__=='__main__':
+    app1.run(debug=True)
+
+# #<img src="{{ url_for('static', filename='1.png') }}">
